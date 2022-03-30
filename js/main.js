5ebccd1b9b2f509e647a25ddde80421b3907301e
niksus-src <73141739+niksus-src@users.noreply.github.com>
2022-03-30 15:13:09 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// 3D
+
+window.scrollTo(0, 1)
+
+let zSpacing = -1000,
+    lastPos = zSpacing / 5,
+    $frames = document.getElementsByClassName('frame'),
+    frames = Array.from($frames),
+    zVals = []
+
+window.onscroll = () => {
+    let top = document.documentElement.scrollTop,
+        delta = lastPos - top
+
+    lastPos = top
+
+    frames.forEach((n, i) => {
+        if(lastPos < 2300)
+        { 
+            zVals.push((i * zSpacing) + zSpacing)
+            zVals[i] += delta * -5
+            let frame = frames[i],
+                transform = `translateZ(${zVals[i]}px)`
+                opacity = zVals[i] < Math.abs(zSpacing) / 1.8 ? 1 : 0
+
+            frame.setAttribute('style', `transform: ${transform}; opacity: ${opacity}`)
+        }
+    })
+}
+
+
